b7f4c09defbb8029da67f93c0f87361878159af3
nikitazlain <kilimangara@yandex.ru>
2017-11-07 00:53:43 +0300

    редакс миддлвары, эндпоинты и создание стора

---
+++ src/api.js
@@ -0,0 +1,6 @@
+import trivialRedux from 'trivial-redux'
+import auth from './endpoints/auth'
+
+export default trivialRedux{
+  auth
+}
---
+++ src/middlewares/urlMiddleware.js
@@ -0,0 +1,20 @@
+import {cloneDeep} from 'lodash'
+
+let isValidFetchingAction = (action) => action.types && action.meta && action.meta.fetch && action.meta.fetch.url
+
+let toEndPointUrl = ( url) => `https://attemka.ru/${url.substr(1)}`
+
+export default (store) => (next) => (action) => {
+
+  if(isValidFetchingAction(action) && action.meta.fetch.url[0] == '~')
+  {
+    const {auth} = store.getState()
+    action = cloneDeep(action)
+    action.meta.fetch.url = toEndPointUrl(loginData, action.meta.fetch.url)
+    action.meta.fetch.headers = {
+      'Authorization': 'Token ' + auth.token,
+    }
+    return next(action)
+  }
+   return next(action)
+}
