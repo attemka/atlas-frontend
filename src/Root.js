346d4dc30c327db432f9f3ef54d2f0cfc374fd4b
Artem <attemka@gmail.com>
2017-11-12 14:44:40 +0300

    changed code style via prettier

--- src/Root.js
+++ src/Root.js
@@ -1,32 +1,29 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import setUpStore from './store'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import setUpStore from "./store";
 
-import Routes from './routes';
-import App from './containers/App';
+import Routes from "./routes";
+import App from "./containers/App";
 
 class Root extends Component {
-
-  componentWillMount = ()=>{
-    const store = setUpStore()
-    this.setState({store})
-  }
+  componentWillMount = () => {
+    const store = setUpStore();
+    this.setState({ store });
+  };
 
   render() {
-    if (!this.state.store) return (<div>Loading...</div>)
+    if (!this.state.store) return <div>Loading...</div>;
     return (
       <Provider store={this.state.store}>
-          <Router>
-            <Switch>
-              <App>
-                {Routes}
-              </App>
-            </Switch>
-          </Router>
+        <Router>
+          <Switch>
+            <App>{Routes}</App>
+          </Switch>
+        </Router>
       </Provider>
-    )
+    );
   }
 }
 
--- src/middlewares/urlMiddleware.js
+++ src/middlewares/urlMiddleware.js
@@ -1,20 +1,18 @@
-import {cloneDeep} from 'lodash'
+import { cloneDeep } from "lodash";
 
-let isValidFetchingAction = (action) => action.types && action.meta && action.meta.fetch && action.meta.fetch.url
+let isValidFetchingAction = action => action.types && action.meta && action.meta.fetch && action.meta.fetch.url;
 
-let toEndPointUrl = ( url) => `https://attemka.ru/${url.substr(1)}`
+let toEndPointUrl = url => `https://attemka.ru/${url.substr(1)}`;
 
-export default (store) => (next) => (action) => {
-
-  if(isValidFetchingAction(action) && action.meta.fetch.url[0] == '~')
-  {
-    const {auth} = store.getState()
-    action = cloneDeep(action)
-    action.meta.fetch.url = toEndPointUrl(loginData, action.meta.fetch.url)
+export default store => next => action => {
+  if (isValidFetchingAction(action) && action.meta.fetch.url[0] == "~") {
+    const { auth } = store.getState();
+    action = cloneDeep(action);
+    action.meta.fetch.url = toEndPointUrl(loginData, action.meta.fetch.url);
     action.meta.fetch.headers = {
-      'Authorization': 'Token ' + auth.token,
-    }
-    return next(action)
+      Authorization: "Token " + auth.token
+    };
+    return next(action);
   }
-   return next(action)
-}
+  return next(action);
+};
